cd0b8d080c5e2e0c0b3f959b82a09d208b23701e
Antonio Vukalovic <avukalov.develop@gmail.com>
2025-06-16 15:23:01 +0200

    postavljanje nuxt3 tailwind daisyUI icons

---
+++ frontend/dev-interview-task/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-6xl font-bold underline">Hello World with TailwindCSS</h1>
+    <button class="btn btn-secondary">
+      Secondary
+      <Icon name="tabler:device-desktop" style="color: white" />
+    </button>
+  </div>
+</template>
---
+++ frontend/dev-interview-task/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
